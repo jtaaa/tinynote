a7832fbdec9ad2fed04163802da75046f7eef949
jtaaa <joshuatallum@gmail.com>
2019-12-31 14:33:53 -0500

    Add GlobalStyles for button normalization

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,11 @@ import * as serviceWorker from './serviceWorker';
 // theme stuff
 import { ThemeProvider } from 'styled-components/macro';
 import theme from './theme';
+import GlobalStyle from 'GlobalStyle';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>,
   document.getElementById('root'),

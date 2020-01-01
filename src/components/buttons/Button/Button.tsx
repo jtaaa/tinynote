eef0d01ebf3628076f8de92f9218937329f00a2a
jtaaa <joshuatallum@gmail.com>
2020-01-01 15:36:00 -0500

    Add Button variants, tests and stories

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,14 +1,34 @@
 import styled from 'styled-components/macro';
 import UnstyledButton from '../UnstyledButton';
 import css from '@styled-system/css';
+import { variant } from 'styled-system';
 
-export const Button = styled(UnstyledButton)(
+type ButtonProps = {
+  variant?: 'primary' | 'secondary';
+};
+export const Button = styled(UnstyledButton)<ButtonProps>(
   css({
-    color: 'primary',
     borderRadius: 4,
     border: '1px solid',
+    color: 'primary',
     '&:hover, &:focus': {
       bg: 'primary/light',
+    },
+  }),
+  variant({
+    variants: {
+      primary: {
+        color: 'primary',
+        '&:hover, &:focus': {
+          bg: 'primary/light',
+        },
+      },
+      secondary: {
+        color: 'secondary',
+        '&:hover, &:focus': {
+          bg: 'secondary/light',
+        },
+      },
     },
   }),
 );

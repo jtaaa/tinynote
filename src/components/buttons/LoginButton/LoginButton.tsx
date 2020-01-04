6308dfc0b0ce1d795abaa70e90512051fe271625
jtaaa <joshuatallum@gmail.com>
2020-01-04 17:20:25 -0500

    Add Login overlay
    Fixes #1, fixes #2, fixes #3, fixes #4

--- src/components/buttons/LoginButton/LoginButton.tsx
+++ src/components/buttons/LoginButton/LoginButton.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import Button from '../Button';
+import React, { useState } from 'react';
+import Overlay from 'components/Overlay';
+import Centered from 'components/Centered';
+import Box from 'components/Box';
+import Divider from 'components/Divider';
+import Button from 'components/buttons/Button';
+import GreatPrimer from 'components/text/GreatPrimer';
+import { useAuth } from 'modules/firebase';
 import { useTranslation } from 'utils/i18next';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import uiConfig from './uiConfig';
 
 const LoginButton = () => {
   const { Trans } = useTranslation('LoginButton');
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const auth = useAuth();
 
   return (
     <>
-      <Button>
+      <Button onClick={() => setOverlayVisible(true)}>
         <Trans i18nKey="login">Login</Trans>
       </Button>
+      <Overlay visible={overlayVisible} setVisible={setOverlayVisible}>
+        <Centered>
+          <Box width={[1, 400]} p={3}>
+            <GreatPrimer textAlign="center">
+              Sign in or create an account
+            </GreatPrimer>
+            <Divider />
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+          </Box>
+        </Centered>
+      </Overlay>
     </>
   );
 };
---
+++ src/components/buttons/LoginButton/uiConfig.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful.
+  // Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google, Facebook and email as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
+export default uiConfig;

c33f9559dda96fad48b81f954d0a51d8c49fd458
jtaaa <joshuatallum@gmail.com>
2020-01-06 15:19:45 -0500

    Add translation

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Box from 'components/Box';
+import { useTranslation } from 'utils/i18next';
 
 const Loading: React.FC = () => {
-  return <Box>Loading...</Box>;
+  const { Trans } = useTranslation('Loading');
+  return (
+    <Box>
+      <Trans i18nKey="loading">Loading...</Trans>
+    </Box>
+  );
 };
 
 export default Loading;

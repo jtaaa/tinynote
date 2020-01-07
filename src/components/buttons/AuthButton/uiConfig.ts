82b7b2408a2434a14d9de44f3af93e2734e31fa3
jtaaa <joshuatallum@gmail.com>
2020-01-07 17:49:22 -0500

    Redirect to HomePage on login

--- src/components/buttons/AuthButton/uiConfig.ts
+++ src/components/buttons/AuthButton/uiConfig.ts
@@ -4,9 +4,9 @@ import firebase from 'firebase/app';
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful.
+  // Redirect to "/" after sign in is successful.
   // Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
+  signInSuccessUrl: '/',
   // We will display Google, Facebook and email as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,

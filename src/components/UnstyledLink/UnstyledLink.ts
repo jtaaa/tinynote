b7dbf491131b7ce32f68afe8c9c0037f6fac956a
jtaaa <joshuatallum@gmail.com>
2020-01-07 16:17:14 -0500

    Add UnstyledLink

---
+++ src/components/UnstyledLink/UnstyledLink.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+
+const UnstyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: inherit;
+`;
+
+export default UnstyledLink;

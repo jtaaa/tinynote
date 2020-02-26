20e05e9c8ba0ece562ee292936e734c00af76825
Joshua Allum <joshuatallum@gmail.com>
2020-02-25 19:24:03 -0500

    Fix storybook for header

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { configure, addDecorator } from '@storybook/react';
 import ThemeDecorator from 'utils/theme/ThemeDecorator';
 
-addDecorator(Story => <ThemeDecorator><Story /></ThemeDecorator>);
+addDecorator(Story => <ThemeDecorator><MemoryRouter><Story /></MemoryRouter></ThemeDecorator>);
 
 configure(require.context('../src', true, /\.stories\.tsx?$/), module);
